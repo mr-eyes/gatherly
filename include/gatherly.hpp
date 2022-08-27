74ec3d6592ec02a48f802969572661d8e6e5529a
mr-eyes <mabuelanin@gmail.com>
2022-08-28 01:47:02 +0200

    checkpoint

--- include/gatherly.hpp
+++ include/gatherly.hpp
@@ -9,19 +9,22 @@
 
 using namespace std;
 
-using PHMAP_COLOR_TO_ID = gtl::flat_hash_map<uint64_t, gtl::flat_hash_set<uint32_t>>;
 using PHMAP_COLOR_TO_COUNT = gtl::flat_hash_map<uint64_t, uint64_t>;
 using PHMAP_ID_TO_NAME = gtl::flat_hash_map<uint64_t, string>;
 using PHMAP_ID_TO_KMER_COUNT = gtl::flat_hash_map<uint64_t, uint64_t>;
 using PHMAP_COLOR_TO_IDS = gtl::flat_hash_map<uint64_t, gtl::flat_hash_set<uint32_t>>;
-
+using PHMAP_KMER_TO_COLOR = gtl::parallel_flat_hash_map<uint64_t, uint64_t,
+    std::hash<uint64_t>, std::equal_to<uint64_t>, std::allocator<std::pair<const uint64_t, uint64_t>>, 2>;
 
 namespace Gatherly {
 
     struct IndexStruct {
-        string color_to_source;
+        string color_to_sources;
         string kmer_to_color;
         string color_to_count;
+        PHMAP_COLOR_TO_IDS* color_to_ids_map;
+        PHMAP_KMER_TO_COLOR* kmer_to_color_map;
+        PHMAP_COLOR_TO_COUNT* color_to_count_map;
     };
 
     class SplittedIndex {
@@ -31,17 +34,67 @@ namespace Gatherly {
         string input_prefix;
         string metadata_file;
         int total_parts;
+
+        // Scale param used while indexing
         int scale;
-        vector<IndexStruct> index_parts;
+
+        // stores the split index (complete or partial).
+        gtl::flat_hash_map<int, IndexStruct> index_parts;
+
+        // Map source id to its original name.
         PHMAP_ID_TO_NAME id_to_name;
+
+        // Map source id to its unique canonical kmer count.
         PHMAP_ID_TO_KMER_COUNT id_to_kmer_count;
 
+        // map that maps the part id to hash intervals<from_hash, to_hash>
+        gtl::flat_hash_map<int, tuple<uint64_t, uint64_t>> hash_ranges;
+
+        // Class constructor
         SplittedIndex(string directory, string input_prefix);
-        void load_colors_to_ids(const std::string& filename, PHMAP_COLOR_TO_IDS* map);
-        void load_color_to_count(const std::string& filename, PHMAP_COLOR_TO_COUNT* map);
-        void load_id_to_name(const std::string& filename); // done
+
+        // Function to deserialize phmap of color to set of ids
+        void load_colors_to_ids(int part_id);
+        
+        // deserialize phmap of colors to count of colors.
+        void load_color_to_count(int part);
+
+        // deserialize phmap of id to name (namesmap)
+        void load_id_to_name(const std::string& filename);
+
+        // deserialize phmap of id to kmer count (used in containment calculation)
         void load_id_to_kmer_count(const std::string& filename); // done
+
+        // deserialize index-part hashed kmer to color phmap
+        void load_kmer_to_color(int part_id);
+
+        // parse metadata file
         tuple<uint64_t, uint64_t> load_metadata(const std::string& filename);
+
+        // load single index part (color->count, color->sources, kmer->color)
+        void load_single_part(int part_id);
+
+        // load all parts
+        void load_all_parts();
+
+        // given a source ID, return the kmer count
+        uint64_t get_kmer_count(int& source_id);
+
+        // given a hashed_kmer, return its color
+        uint64_t get_color_from_hash(uint64_t& kmer_hash);
+
+        // given a hashed_kmer, return its associated sources
+        gtl::flat_hash_set<uint32_t> get_ids_from_hash(uint64_t& kmer_hash);
+
+        // given a hashed_kmer, return its associated sources
+        gtl::flat_hash_set<uint32_t> get_ids_from_color(uint64_t& kmer_hash, int part_id);
+
+        // given a scale value and number of pars, split the hash-space into equal hash intervals.
+        void splitted_ranges(uint64_t scale, int parts);
+
+        // TODO: Optimize to interval search
+        int kmer_to_part(uint64_t& kmer_hash);
+
 
     };
 
--- src/gatherly.cpp
+++ src/gatherly.cpp
@@ -56,27 +56,91 @@ void Gatherly::SplittedIndex::load_id_to_name(const std::string& filename) {
     fin.close();
 }
 
-void Gatherly::SplittedIndex::load_color_to_count(const std::string& filename, PHMAP_COLOR_TO_COUNT* map) {
-    gtl::BinaryInputArchive ar_in_colorsCount(filename.c_str());
-    map->phmap_load(ar_in_colorsCount);
-    assert(map->size());
+void Gatherly::SplittedIndex::load_color_to_count(int part_id) {
+    string filePath = this->index_parts[part_id].color_to_count;
+    gtl::BinaryInputArchive ar_in_colorsCount(filePath.c_str());
+    this->index_parts[part_id].color_to_count_map->phmap_load(ar_in_colorsCount);
+    assert(this->index_parts[part_id].color_to_count_map->size());
 }
 
-// Kinda slow, but noway.
-void Gatherly::SplittedIndex::load_colors_to_ids(const std::string& filename, PHMAP_COLOR_TO_IDS* map) {
-    gtl::BinaryInputArchive ar_in(filename.c_str());
+// Kinda slow, but no alternative.
+void Gatherly::SplittedIndex::load_colors_to_ids(int part_id) {
+    string filePath = this->index_parts[part_id].color_to_sources;
+    gtl::BinaryInputArchive ar_in(filePath.c_str());
     size_t size;
     ar_in.loadBinary(&size);
-    map->reserve(size);
+    this->index_parts[part_id].color_to_ids_map->reserve(size);
     while (size--)
     {
         uint64_t k;
         gtl::flat_hash_set<uint32_t> v;
         ar_in.loadBinary(&k);
         ar_in.loadBinary(&v);
-        map->insert_or_assign(std::move(k), std::move(v));
+        this->index_parts[part_id].color_to_ids_map->insert_or_assign(std::move(k), std::move(v));
     }
 }
+
+void Gatherly::SplittedIndex::load_kmer_to_color(int part_id) {
+    string filePath = this->index_parts[part_id].kmer_to_color;
+    gtl::BinaryInputArchive ar_in(filePath.c_str());
+    this->index_parts[part_id].kmer_to_color_map->phmap_load(ar_in);
+}
+
+
+void Gatherly::SplittedIndex::load_single_part(int part_id) {
+    this->load_colors_to_ids(part_id);
+    this->load_kmer_to_color(part_id);
+}
+
+
+void Gatherly::SplittedIndex::load_all_parts() {
+    for (int part_id = 1; part_id <= this->total_parts; part_id++)
+        this->load_single_part(part_id);
+}
+
+uint64_t Gatherly::SplittedIndex::get_kmer_count(int & source_id) {
+    return this->id_to_kmer_count[source_id];
+}
+
+
+void Gatherly::SplittedIndex::splitted_ranges(uint64_t scale, int parts) {
+    uint64_t max_hash = UINT64_MAX / (uint64_t)scale;
+    uint64_t from_hash = 0;
+    uint64_t to_hash = 0;
+    uint64_t step = (uint64_t)(max_hash / parts);
+    for (int i = 1; i <= parts; i++) {
+        to_hash += step;
+        this->hash_ranges[i] = { from_hash, to_hash };
+        from_hash += step;
+    }
+
+    if (to_hash < max_hash)
+        this->hash_ranges[parts] = { get<0>(this->hash_ranges[parts]), max_hash };
+}
+
+int Gatherly::SplittedIndex::kmer_to_part(uint64_t& kmer_hash) {
+    for (auto& [part_id, hash_interval] : this->hash_ranges)
+        if (get<0>(hash_interval) <= kmer_hash && kmer_hash < get<1>(hash_interval))
+            return part_id;
+    return 0;
+}
+
+
+uint64_t Gatherly::SplittedIndex::get_color_from_hash(uint64_t& kmer_hash) {
+    return this->index_parts[kmer_to_part(kmer_hash)].kmer_to_color_map->operator[](kmer_hash);
+}
+
+gtl::flat_hash_set<uint32_t> Gatherly::SplittedIndex::get_ids_from_color(uint64_t &kmer_color, int part_id) {
+    return this->index_parts[part_id].color_to_ids_map->operator[](kmer_color);
+}
+
+gtl::flat_hash_set<uint32_t> Gatherly::SplittedIndex::get_ids_from_hash(uint64_t &kmer_hash) {
+    int inferred_part_id = this->kmer_to_part(kmer_hash);
+    uint64_t kmer_color = this->index_parts[inferred_part_id].kmer_to_color_map->operator[](kmer_hash);
+    return this->get_ids_from_color(kmer_color, inferred_part_id);
+}
+
+
 
 Gatherly::SplittedIndex::SplittedIndex(string directory, string input_prefix) {
     string _file_id_to_name = input_prefix + "_id_to_name.tsv";
@@ -89,17 +153,21 @@ Gatherly::SplittedIndex::SplittedIndex(string directory, string input_prefix) {
     this->total_parts = get<1>(metadata_tup);
     this->load_id_to_kmer_count(_file_id_to_kmer_count);
     this->load_id_to_name(_file_id_to_name);
+    this->splitted_ranges(this->scale, this->total_parts);
+
 
     for (int part_id = 1; part_id <= this->total_parts; part_id++) {
         string part_name = "part" + to_string(part_id);
-        string _file_color_to_sources = input_prefix + "_color_to_sources." + part_name + ".bin";
-        string _file_color_to_count = input_prefix + "_color_count." + part_name + ".bin";
-        string _file_kmer_to_color = input_prefix + "kmer_to_color." + part_name + ".phmap";
+        string _file_color_to_sources = input_prefix.append("_color_to_sources." + part_name + ".bin");
+        string _file_color_to_count = input_prefix.append("_color_count." + part_name + ".bin");
+        string _file_kmer_to_color = input_prefix.append("kmer_to_color." + part_name + ".phmap");
         IndexStruct tmp_IndexStruct;
         tmp_IndexStruct.color_to_count = _file_color_to_count;
-        tmp_IndexStruct.color_to_source = _file_color_to_sources;
+        tmp_IndexStruct.color_to_sources = _file_color_to_sources;
         tmp_IndexStruct.kmer_to_color = _file_kmer_to_color;
-        this->index_parts.emplace_back(tmp_IndexStruct);
+        this->index_parts[part_id] = tmp_IndexStruct;
     }
 
 }
+
+
