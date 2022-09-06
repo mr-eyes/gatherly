3e589ea44c04678e77fa70871d7e1e044e9e8415
mr-eyes <mabuelanin@gmail.com>
2022-09-06 10:30:18 -0700

    test

--- test/test_splittedIndex.py
+++ test/test_splittedIndex.py
@@ -1,13 +1,15 @@
 from pydoc import splitdoc
-import splitted_index
+import gatherly
 
-Gath = splitted_index.SplittedIndex("/home/mabuelanin/dib-dev/gatherly/splitted_index/ok")
-
-print(dir(Gath))
-
+Gath = gatherly.SplittedIndex("/home/mabuelanin/dib-dev/gatherly/splitted_index/ok", 51)
 Gath.load_all_parts()
 
-hh = 20236993234229
-x = Gath.get_sources_from_hash(hh);
+# hh = 20236993234229
+# x = Gath.get_sources_from_hash(hh);
 
-print(x)
+sig_path = "/home/mabuelanin/dib-dev/gatherly/sigs_100k/DRR083176.sig"
+fasta_path = "/home/mabuelanin/dib-dev/gatherly/DRR083176_1.fastq.gz"
+# result = Gath.query_sig(sig_path)
+result = Gath.query_fastx(fasta_path)
+
+print(result)
